6b905aa918020c8f2e68970e685a3a9da9213992
Nishad Moideen <nishadmoideen@yahoo.com>
2018-10-09 18:09:19 -0400

    Initial Checkin

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <!--
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>
+    //-->
+    <app-header v-bind:title="title" v-on:titleChanged='updateTitle($event)'></app-header>
+    <app-ninjas v-bind:ninjas="ninjas"></app-ninjas>
+    <hr/>
+     <app-footer v-bind:title="title"></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
+
+export default {
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas
+  },
+  data () {
+    return {
+      title: 'View Ninjas',
+      ninjas: [
+          {name: 'Yoshi', speciality:'Vue Components', show: false},
+          {name: 'Mario', speciality:'HTML Wizardry', show: false},
+          {name: 'Hitoshi', speciality:'Click Components', show: false},
+          {name: 'Kami', speciality:'Webpack', show: false},
+          {name: 'Tango', speciality:'Data Design', show: false},
+          ]
+    }
+  },
+  methods: {
+    updateTitle: function(newTitle) {
+      this.title=newTitle;
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+    color: purple;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+    <header>
+        <h1 v-on:click="changeTitle">{{title}}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    },
+    data() {
+        return {
+            name: 'View Nothing'
+        };
+    },
+    methods: {
+        changeTitle: function() {
+            //this.title="View Wizards";
+            this.$emit('titleChanged','View Wizards');
+        }
+    }
+
+}
+</script>
+<style scoped>
+header {
+    background-color: lightgreen;
+    padding: 10px;
+}
+h1 {
+    color: #222;
+    text-align: center;
+}
+
+</style>
+
